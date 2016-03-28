f59cb6cf5b3ea3877dd05f882acbab5787d89c9c
InvictusCody57 <mousumaiub10@gmail.com>
2016-03-28 19:14:54 +0600

    emoji added to image edit

--- ImageTalk/UIImage+Scale.m
+++ ImageTalk/UIImage+Scale.m
@@ -23,7 +23,19 @@
     // Return our new scaled image
     return scaledImage;
 }
-
+-(UIImage*) positionToRect:(CGRect)rect {
+    // Create a bitmap graphics context
+    // This will also set it as the current context
+    UIGraphicsBeginImageContext(rect.size);
+    // Draw the scaled image in the current context
+    [self drawInRect:CGRectMake(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)];
+    // Create a new image from current context
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    // Pop the current context from the stack
+    UIGraphicsEndImageContext();
+    // Return our new scaled image
+    return scaledImage;
+}
 
 
 - (UIImage *)scaleImageToSize:(CGSize)newSize {
